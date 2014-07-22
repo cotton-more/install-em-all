d3d4283b306a945dd2c154a86582a41a95887c75
Ivan <vansanblch@users.noreply.github.com>
2014-07-22 11:00:29 +0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+# scirpts to install
+# git-forest - https://raw.githubusercontent.com/jwiegley/git-scripts/master/git-forest
+# git-wtf - https://raw.githubusercontent.com/michaelklishin/git-wtf/master/git-wtf
